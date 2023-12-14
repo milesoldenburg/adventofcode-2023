34a97f55d8b5351110e6a89925b21ab2d5177af8
Miles Oldenburg <milesoldenburg@gmail.com>
2023-12-14 17:56:25 -0500

    Day 13 Part 2 progress

---
+++ 13/2/main.py
@@ -0,0 +1,63 @@
+import difflib
+import functools
+
+
+def find_mirror_point(pattern):
+    potential_mirror_points = []
+    for i, block in enumerate(pattern):
+        if i == 0:
+            # TODO: can this row be changed to match row 1
+            continue
+        else:
+            # TODO: can this row be changed to match the previous row
+            if block == pattern[i - 1]:
+                print(f'Potential mirror between block {i - 1} and {i}')
+                print(block)
+                potential_mirror_points.append(i - 1)
+
+    for potential_mirror_point in potential_mirror_points:
+        if potential_mirror_point == 0:
+            print('Confirmed mirror point between block 0 and 1')
+            print()
+            return potential_mirror_point
+        elif potential_mirror_point == len(pattern) - 2:
+            print(f'Confirmed mirror point between block {len(pattern) - 2} and {len(pattern) - 1}')
+            print()
+            return potential_mirror_point
+        else:
+            blocks_match = True
+            for i in range(potential_mirror_point - 1, -1, -1):
+                compared_blocki = potential_mirror_point + 1 + potential_mirror_point - i
+                print(f'Comparing block {i} and {compared_blocki}')
+                if compared_blocki < len(pattern) and pattern[i] != pattern[compared_blocki]:
+                    blocks_match = False
+                    break
+                # TODO: can one of these rows be changed to match?
+            if blocks_match:
+                print(f'Confirmed mirror point between block {potential_mirror_point} and {potential_mirror_point + 1}')
+                print()
+                return potential_mirror_point
+            else:
+                print('No mirror points in block')
+
+    return None
+
+
+with open('../test.txt') as f:
+    data = f.read()
+    patterns = [x.splitlines() for x in data.split('\n\n')]
+
+summary = 0
+for pattern in patterns:
+    print('Checking rows...')
+    row_mirror_point = find_mirror_point(pattern)
+
+    if row_mirror_point is not None:
+        summary += (row_mirror_point + 1) * 100
+    else:
+        print('Checking columns...')
+        columns = [functools.reduce(lambda x, y: x + y[i], pattern, '') for i in range(len(pattern[0]))]
+        column_mirror_point = find_mirror_point(columns)
+        summary += column_mirror_point + 1
+
+print(summary)
