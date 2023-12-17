601d3c05c1a5e0d2e8db61a782636cf4dca005c7
Miles Oldenburg <milesoldenburg@gmail.com>
2023-12-17 16:04:38 -0500

    Day 16 Part 1

--- 16/1/main.py
+++ 16/1/main.py
@@ -1,3 +1,4 @@
+from collections import deque
 from enum import Enum
 import numpy as np
 
@@ -9,72 +10,95 @@ class Direction(Enum):
     LEFT = 4
 
 
-def process_laser(grid, energized_grid, visited_squares, row: int, column: int, direction: Direction):
-    if (row, column, direction) in visited_squares or row < 0 or column < 0 or row >= grid.shape[0] or column >= grid.shape[0]:
-        return
+def go_up(q, visited_squares, row, column):
+    if row > 0 and (row - 1, column, Direction.UP) not in visited_squares:
+        q.appendleft((row - 1, column, Direction.UP))
+
+
+def go_right(q, visited_squares, shape, row, column):
+    if column < shape[1] - 1 and (row, column + 1, Direction.RIGHT) not in visited_squares:
+        q.appendleft((row, column + 1, Direction.RIGHT))
+
+
+def go_down(q, visited_squares, shape, row, column):
+    if row < shape[0] - 1 and (row + 1, column, Direction.DOWN) not in visited_squares:
+        q.appendleft((row + 1, column, Direction.DOWN))
+
+
+def go_left(q, visited_squares, row, column):
+    if column > 0 and (row, column - 1, Direction.LEFT) not in visited_squares:
+        q.appendleft((row, column - 1, Direction.LEFT))
+
+
+def process_laser(q, grid, energized_grid, visited_squares, row: int, column: int, direction: Direction):
     energized_grid[row, column] = 1
     e = grid[row, column]
     visited_squares.append((row, column, direction))
+
     if e == '.':
         if direction == Direction.UP:
-            process_laser(grid, energized_grid, visited_squares, row - 1, column, Direction.UP)
+            go_up(q, visited_squares, row, column)
         elif direction == Direction.RIGHT:
-            process_laser(grid, energized_grid, visited_squares, row, column + 1, Direction.RIGHT)
+            go_right(q, visited_squares, grid.shape, row, column)
         elif direction == Direction.DOWN:
-            process_laser(grid, energized_grid, visited_squares, row + 1, column, Direction.DOWN)
+            go_down(q, visited_squares, grid.shape, row, column)
         elif direction == Direction.LEFT:
-            process_laser(grid, energized_grid, visited_squares, row, column - 1, Direction.LEFT)
+            go_left(q, visited_squares, row, column)
     elif e == '|':
         if direction == Direction.UP:
-            process_laser(grid, energized_grid, visited_squares, row - 1, column, Direction.UP)
+            go_up(q, visited_squares, row, column)
         elif direction == Direction.RIGHT:
-            process_laser(grid, energized_grid, visited_squares, row - 1, column, Direction.UP)
-            process_laser(grid, energized_grid, visited_squares, row + 1, column, Direction.DOWN)
+            go_up(q, visited_squares, row, column)
+            go_down(q, visited_squares, grid.shape, row, column)
         elif direction == Direction.DOWN:
-            process_laser(grid, energized_grid, visited_squares, row + 1, column, Direction.DOWN)
+            go_down(q, visited_squares, grid.shape, row, column)
         elif direction == Direction.LEFT:
-            process_laser(grid, energized_grid, visited_squares, row - 1, column, Direction.UP)
-            process_laser(grid, energized_grid, visited_squares, row + 1, column, Direction.DOWN)
+            go_up(q, visited_squares, row, column)
+            go_down(q, visited_squares, grid.shape, row, column)
     elif e == '\\':
         if direction == Direction.UP:
-            process_laser(grid, energized_grid, visited_squares, row, column - 1, Direction.LEFT)
+            go_left(q, visited_squares, row, column)
         elif direction == Direction.RIGHT:
-            process_laser(grid, energized_grid, visited_squares, row + 1, column, Direction.DOWN)
+            go_down(q, visited_squares, grid.shape, row, column)
         elif direction == Direction.DOWN:
-            process_laser(grid, energized_grid, visited_squares, row, column + 1, Direction.RIGHT)
+            go_right(q, visited_squares, grid.shape, row, column)
         elif direction == Direction.LEFT:
-            process_laser(grid, energized_grid, visited_squares, row - 1, column, Direction.UP)
+            go_up(q, visited_squares, row, column)
     elif e == '/':
         if direction == Direction.UP:
-            process_laser(grid, energized_grid, visited_squares, row, column + 1, Direction.RIGHT)
+            go_right(q, visited_squares, grid.shape, row, column)
         elif direction == Direction.RIGHT:
-            process_laser(grid, energized_grid, visited_squares, row - 1, column, Direction.UP)
+            go_up(q, visited_squares, row, column)
         elif direction == Direction.DOWN:
-            process_laser(grid, energized_grid, visited_squares, row, column - 1, Direction.LEFT)
+            go_left(q, visited_squares, row, column)
         elif direction == Direction.LEFT:
-            process_laser(grid, energized_grid, visited_squares, row + 1, column, Direction.DOWN)
+            go_down(q, visited_squares, grid.shape, row, column)
     elif e == '-':
         if direction == Direction.UP:
-            process_laser(grid, energized_grid, visited_squares, row, column - 1, Direction.LEFT)
-            process_laser(grid, energized_grid, visited_squares, row, column + 1, Direction.RIGHT)
+            go_left(q, visited_squares, row, column)
+            go_right(q, visited_squares, grid.shape, row, column)
         elif direction == Direction.RIGHT:
-            process_laser(grid, energized_grid, visited_squares, row, column + 1, Direction.RIGHT)
+            go_right(q, visited_squares, grid.shape, row, column)
         elif direction == Direction.DOWN:
-            process_laser(grid, energized_grid, visited_squares, row, column - 1, Direction.LEFT)
-            process_laser(grid, energized_grid, visited_squares, row, column + 1, Direction.RIGHT)
+            go_left(q, visited_squares, row, column)
+            go_right(q, visited_squares, grid.shape, row, column)
         elif direction == Direction.LEFT:
-            process_laser(grid, energized_grid, visited_squares, row, column - 1, Direction.LEFT)
+            go_left(q, visited_squares, row, column)
 
 
 data = []
-for line in open('../test.txt'):
+for line in open('../input.txt'):
     data.append([*line.strip()])
 grid = np.array(data)
 energized_grid = np.zeros_like(grid, dtype=int)
 visited_squares = []
 print(grid)
 
-process_laser(grid, energized_grid, visited_squares, 0, 0, Direction.RIGHT)
+q = deque([(0, 0, Direction.RIGHT)])
+
+while q:
+    value = q.popleft()
+    process_laser(q, grid, energized_grid, visited_squares, value[0], value[1], value[2])
 
 print(energized_grid)
 print(np.sum(energized_grid))
