80af434647e32538a885dbe3df6df2458c8e0230
Miles Oldenburg <moldenbu@cisco.com>
2023-12-19 21:57:27 -0500

    Day 19 Part 1

--- 19/1/main.py
+++ 19/1/main.py
@@ -1,12 +1,77 @@
 import re
 
-workflow_pattern = re.compile(r'(\w+){(.*)}')
+workflows_pattern = re.compile(r'(\w+){(.*)}')
+workflow_pattern = re.compile(r'([xmas])([<>])(\d+):(\w+)')
+xmas_pattern = re.compile(r'{x=(\d+),m=(\d+),a=(\d+),s=(\d+)}')
 
-for line in open('../test_a.txt'):
-    m = re.match(workflow_pattern, line)
+workflows = {}
+for line in open('../input_a.txt'):
+    m = re.match(workflows_pattern, line)
     title, flows_text = m.groups()
-    print(title)
     flows = flows_text.split(',')
-    print(flows[0:-1])
     otherwise = flows[-1]
-    print(otherwise)
+    rules = []
+    for f in flows[0:-1]:
+        m = re.match(workflow_pattern, f)
+        part, comparator, value, w = m.groups()
+        value = int(value)
+        rules.append((part, comparator, value, w))
+    workflows[title] = {
+        'rules': rules,
+        'otherwise': otherwise
+    }
+
+accepted_parts_total = 0
+for line in open('../input_b.txt'):
+    match = re.match(xmas_pattern, line)
+    x, m, a, s = match.groups()
+    x = int(x)
+    m = int(m)
+    a = int(a)
+    s = int(s)
+    a_or_r = False
+    workflow_name = 'in'
+    while not a_or_r:
+        workflow = workflows[workflow_name]
+        rule_satisfied = False
+        for rule in workflow.get('rules'):
+            part = x
+            if rule[0] == 'm':
+                part = m
+            elif rule[0] == 'a':
+                part = a
+            elif rule[0] == 's':
+                part = s
+            if rule[1] == '<':
+                rule_satisfied = part < rule[2]
+                if rule_satisfied:
+                    if rule[3] == 'A':
+                        a_or_r = True
+                        accepted_parts_total += x + m + a + s
+                    elif rule[3] == 'R':
+                        a_or_r = True
+                    else:
+                        workflow_name = rule[3]
+                    break
+            elif rule[1] == '>':
+                rule_satisfied = part > rule[2]
+                if rule_satisfied:
+                    if rule[3] == 'A':
+                        a_or_r = True
+                        accepted_parts_total += x + m + a + s
+                    elif rule[3] == 'R':
+                        a_or_r = True
+                    else:
+                        workflow_name = rule[3]
+                    break
+        if not rule_satisfied:
+            otherwise = workflow.get('otherwise')
+            if otherwise == 'A':
+                a_or_r = True
+                accepted_parts_total += x + m + a + s
+            elif otherwise == 'R':
+                a_or_r = True
+            else:
+                workflow_name = otherwise
+
+print(accepted_parts_total)
